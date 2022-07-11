24da255d29289dcac5afd6190e1f94a3189000a1
correa0inaiara <correa0inaiara@gmail.com>
2022-07-11 19:35:20 -0300

    - add footer to project to-do list - fixed nav links styles of home page - add redirect of not found pages to the home page

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var app = express()
 
 app.use(express.static('www'));
 
+app.get('*', function (req, res) {
+	res.redirect('https://naiaracorrea.herokuapp.com/')
+})
+
 var server = app.listen(process.env.PORT || 8000, function () {
 	var host = server.address().address
 	var port = server.address().port
